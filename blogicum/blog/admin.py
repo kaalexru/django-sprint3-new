6f4325f7260adfeacf3da6e69b8c6c42eb94e8ec
kaalex <ka_alex@mail.ru>
2026-09-20 10:34:57 +0300

    Добавлена кастомизация публикаций и пользователей в админке

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.db.models import Count
 
 from .models import Category, Location, Post
 
 
+User = get_user_model()
+
 admin.site.empty_value_display = 'Не задано'
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class BlogicumUserAdmin(UserAdmin):
+    list_display = (*UserAdmin.list_display, 'posts_count')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            posts_count=Count('posts'),
+        )
+
+    @admin.display(
+        description='Количество публикаций',
+        ordering='posts_count',
+    )
+    def posts_count(self, user):
+        return user.posts_count
 
 
 @admin.register(Post)
